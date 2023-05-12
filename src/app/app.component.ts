6216b7a97988bae532ae1b2cdc2286df8ad88656
Scott Phounpadith <sphounpadith@h4tg51svq6x3.acad.stclaircollege.scc>
2023-05-12 15:14:04 -0400

    sample of switching selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-new',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'spring2023';
+  title = 'learningInClass';
 }
